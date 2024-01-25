bb212515d9462fed106a38760ec31716cfbc808b
ro202621 <ro202621@ohio.edu>
2024-01-24 19:51:48 -0500

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ryan Oliveri
+ *        Date: 1/24/2024
+ *      Description: Small program just for testing git
+ *      Git Hub things!
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+
+    return 0;
+}// main
